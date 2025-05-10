25c7ae31e0f17c64984e4eb0f508b3ed2654d834
unisanta <unisanta@santacecilia.br>
2025-05-09 21:27:29 -0300

    cursor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Tarefa } from "./tarefa";
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, retry } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,40 +12,91 @@ import { HttpClient } from '@angular/common/http';
 })
 export class AppComponent {
   title = 'TODOapp';
-
   arrayDeTarefas: Tarefa[] = [];
-  apiURL : string;
+  apiURL: string;
+  errorMessage: string = '';
 
-constructor(private http: HttpClient) {
-  this.apiURL = 'https://back-todoapp-om3r.onrender.com';
-this.READ_tarefas();
-}
-
-CREATE_tarefa(descricaoNovaTarefa: string) {
-  var novaTarefa = new Tarefa(descricaoNovaTarefa, false);
-  this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa).subscribe(
-    resultado => { console.log(resultado); this.READ_tarefas(); });
+  constructor(private http: HttpClient) {
+    this.apiURL = 'https://back-todoapp-om3r.onrender.com';
+    this.READ_tarefas();
   }
 
-READ_tarefas() {
-  this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`).subscribe(
-    resultado => this.arrayDeTarefas=resultado);
-   
-}
-DELETE_tarefa(tarefaAserRemovida : Tarefa) {
-  var indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
- var id = this.arrayDeTarefas[indice]._id;
- this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`).subscribe(
- resultado => { console.log(resultado); this.READ_tarefas(); });
- }
+  private handleError(error: HttpErrorResponse) {
+    this.errorMessage = 'Ocorreu um erro na operação. Por favor, tente novamente.';
+    console.error('Erro:', error);
+    return throwError(() => error);
+  }
 
- UPDATE_tarefa(tarefaAserModificada: Tarefa) {
-  var indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
-  var id = this.arrayDeTarefas[indice]._id;
-  this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`,
-  tarefaAserModificada).subscribe(
-  resultado => { console.log(resultado); this.READ_tarefas(); });
- }
- 
+  CREATE_tarefa(descricaoNovaTarefa: string) {
+    const novaTarefa = new Tarefa(descricaoNovaTarefa, false);
+    this.http.post<Tarefa>(`${this.apiURL}/api/post`, novaTarefa)
+      .pipe(
+        retry(3),
+        catchError(this.handleError.bind(this))
+      )
+      .subscribe({
+        next: (resultado) => {
+          console.log('Tarefa criada:', resultado);
+          this.READ_tarefas();
+        },
+        error: (error) => {
+          console.error('Erro ao criar tarefa:', error);
+        }
+      });
+  }
 
+  READ_tarefas() {
+    this.http.get<Tarefa[]>(`${this.apiURL}/api/getAll`)
+      .pipe(
+        retry(3),
+        catchError(this.handleError.bind(this))
+      )
+      .subscribe({
+        next: (resultado) => {
+          this.arrayDeTarefas = resultado;
+          console.log('Tarefas carregadas:', resultado);
+        },
+        error: (error) => {
+          console.error('Erro ao carregar tarefas:', error);
+        }
+      });
+  }
+
+  DELETE_tarefa(tarefaAserRemovida: Tarefa) {
+    const indice = this.arrayDeTarefas.indexOf(tarefaAserRemovida);
+    const id = this.arrayDeTarefas[indice]._id;
+    this.http.delete<Tarefa>(`${this.apiURL}/api/delete/${id}`)
+      .pipe(
+        retry(3),
+        catchError(this.handleError.bind(this))
+      )
+      .subscribe({
+        next: (resultado) => {
+          console.log('Tarefa removida:', resultado);
+          this.READ_tarefas();
+        },
+        error: (error) => {
+          console.error('Erro ao remover tarefa:', error);
+        }
+      });
+  }
+
+  UPDATE_tarefa(tarefaAserModificada: Tarefa) {
+    const indice = this.arrayDeTarefas.indexOf(tarefaAserModificada);
+    const id = this.arrayDeTarefas[indice]._id;
+    this.http.patch<Tarefa>(`${this.apiURL}/api/update/${id}`, tarefaAserModificada)
+      .pipe(
+        retry(3),
+        catchError(this.handleError.bind(this))
+      )
+      .subscribe({
+        next: (resultado) => {
+          console.log('Tarefa atualizada:', resultado);
+          this.READ_tarefas();
+        },
+        error: (error) => {
+          console.error('Erro ao atualizar tarefa:', error);
+        }
+      });
+  }
 }
